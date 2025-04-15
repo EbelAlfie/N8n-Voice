644d3a4ad224c1caefaa84149df523583ce7760f
Davis Inde Satya <davis.onde.wei@gmail.com>
2025-04-16 01:24:08 +0700

    ws and http url

--- app/src/main/java/com/app/customerservice/di/HttpClient.kt
+++ app/src/main/java/com/app/customerservice/di/HttpClient.kt
@@ -2,18 +2,31 @@ package com.app.customerservice.di
 
 import com.app.customerservice.data.service.ApiService
 import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.WebSocket
+import okhttp3.WebSocketListener
 import retrofit2.Retrofit
 
 object HttpClient {
 
-  private val retrofit: Retrofit by lazy {
-    val okHttp = OkHttpClient
+  private val okHttpClient: OkHttpClient by lazy {
+    OkHttpClient
       .Builder()
       .build()
+  }
 
+  private val retrofit: Retrofit by lazy {
     Retrofit.Builder()
-      .client(okHttp)
+      .client(okHttpClient)
+      .baseUrl("http://localhost:5678/")
       .build()
+  }
+
+  fun provideWebSocket(socketListener: WebSocketListener): WebSocket {
+    val request = Request.Builder()
+      .url("ws://localhost:8081/")
+      .build()
+    return okHttpClient.newWebSocket(request, socketListener)
   }
 
   fun provideApiService(): ApiService {
